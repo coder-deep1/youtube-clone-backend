eca325af91bf9528fe564ab9cea6ff0a2264b63c
Deepansh Gangwar <deepanshgangwar7037@gmail.com>
2023-11-14 22:51:18 +0530

    middleware setup

---
+++ src/utils/cloudniary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
+  api_key: "process.env.CLOUDINARY_API_KEY",
+  api_secret: "process.env.CLOUDINARY_API_SECRET",
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file uploaded successfully cloudinary
+    console.log("file uploaded successfully cloudinary");
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove file from local storage but than operation is not successful
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
