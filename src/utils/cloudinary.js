fdf6626d4a7724059187e97be50f500acf307446
Deepansh Gangwar <deepanshgangwar7037@gmail.com>
2023-12-18 22:33:22 +0530

    logout login user

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+// cloudinary.config({
+//   cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
+//   api_key: "process.env.CLOUDINARY_API_KEY",
+//   api_secret: "process.env.CLOUDINARY_API_SECRET",
+// });
 cloudinary.config({
-  cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
-  api_key: "process.env.CLOUDINARY_API_KEY",
-  api_secret: "process.env.CLOUDINARY_API_SECRET",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -15,7 +20,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file uploaded successfully cloudinary
-    console.log("file uploaded successfully cloudinary");
+    // console.log("file uploaded successfully cloudinary");
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove file from local storage but than operation is not successful
