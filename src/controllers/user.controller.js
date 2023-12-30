07f5b0fbd8e6646f25561c1ac85383fd82edbf28
Deepansh Gangwar <deepanshgangwar7037@gmail.com>
2023-12-30 16:27:50 +0530

    Fix coverImageLocalPath assignment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -46,7 +46,17 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   // Add these lines for debugging
   // console.log("req.files:", req.files);
