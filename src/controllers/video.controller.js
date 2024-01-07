12ef5dc26b357dafd0ce40f2945b010a6f03c1b9
Deepansh Gangwar <deepanshgangwar7037@gmail.com>
2024-01-07 18:57:53 +0530

    Add new controllers and models for comments, likes, playlists, tweets, and videos

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,4 @@
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
---
+++ src/routes/video.routes.js
@@ -0,0 +1,2 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
